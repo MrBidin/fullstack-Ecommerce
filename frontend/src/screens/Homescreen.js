7fbc653c1a62564601d4e12cce0bd6c3bf7fbabd
MrBidin <ahmatfirdaus.works@gmail.com>
2023-02-15 12:52:48 +0700

    menambahkan redux sebagai state management pada halaman utama dan details

--- frontend/src/screens/Homescreen.js
+++ frontend/src/screens/Homescreen.js
@@ -1,29 +1,33 @@
 // import products from '../products';
 import {Row, Col} from 'react-bootstrap';
 import Product from '../components/Product';
-import { useState, useEffect } from "react";
-import axios from 'axios';
+import { useEffect } from "react";
+import {listProducts} from '../actions/productActions';
+import { useDispatch, useSelector } from 'react-redux';
+import Message from '../components/Message';
+import Loader from '../components/Loader';
 
 const Homescreen = () => {
-  const [products, setProducts] = useState([]);
+  const dispatch = useDispatch();
+  const productList = useSelector(state => state.productList);
+  const {error, products, loading} = productList;
+
   useEffect(() => {
-    const fetchProducts = async () => {
-      const {data} = await axios.get('/api/v1/products');
-      setProducts(data)
-    }
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
   return (
     <>
     <h1>The latest Products</h1>
-    <Row>
+    { loading ? <Loader /> : error ? <Message variant='danger'>{error}</Message> : 
+      <Row>
       {products.map((product) => (
-       (
+      (
         <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
           <Product product={product} />
         </Col>
       )))}
-    </Row>
+      </Row>
+    }
     </>
   )
 };
--- frontend/src/screens/ProductScreen.js
+++ frontend/src/screens/ProductScreen.js
@@ -1,79 +1,80 @@
 import { useParams, Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Card, Button } from "react-bootstrap";
-// import products from '../products';
 import Rating from "../components/Rating";
-import { useEffect, useState } from "react";
-import axios from 'axios';
+import { useEffect} from "react";
+import { listProductDetails } from "../actions/productActions";
+import { useSelector, useDispatch } from 'react-redux';
+import Message from "../components/Message";
+import Loader from "../components/Loader";
 
 const ProductScreen = () => {
 
-  const {id} = useParams();
+  const dispatch = useDispatch()
+  const productDetails = useSelector(state => state.productDetails);
+  const {product, error, loading} = productDetails;
 
-  const [product, setProduct] = useState({});
+  const {id} = useParams();
   
   useEffect(() => {
-    const fetchProduct = async () => {
-      const {data} = await axios.get(`/api/v1/products/${id}`);
-      setProduct(data)
-    }
-    fetchProduct()
-  }, [id]);
+    dispatch(listProductDetails(id))
+  }, [id, dispatch]);
 
   return (
     <>
       <Link to='/' className="btn btn-light my-3">Go back</Link>
+      {loading ? <Loader /> : error ? <Message variant="danger">{error}</Message> : 
       <Row>
-        <Col md={6}>
-          <Image src={product.image} alt={product.name} fluid/>
-        </Col>
-        <Col md={3}>
+      <Col md={6}>
+        <Image src={product.image} alt={product.name} fluid/>
+      </Col>
+      <Col md={3}>
+        <ListGroup variant='flush'>
+          <ListGroup.Item>
+            <h3>{product.name}</h3>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            <Rating rating={product.rating} numReviewsText={`${product.numReviews} reviews`}/>
+          </ListGroup.Item>
+          <ListGroup.Item>
+            price: ${product.price}
+          </ListGroup.Item>
+          <ListGroup.Item>
+            description: {product.description}
+          </ListGroup.Item>
+        </ListGroup>
+      </Col>
+      <Col md={3}>
+        <Card>
           <ListGroup variant='flush'>
             <ListGroup.Item>
-              <h3>{product.name}</h3>
+              <Row>
+                <Col>
+                  Price: 
+                </Col>
+                <Col>
+                  <strong>${product.price}</strong>
+                </Col>
+              </Row>
             </ListGroup.Item>
             <ListGroup.Item>
-              <Rating rating={product.rating} numReviewsText={`${product.numReviews} reviews`}/>
+              <Row>
+                <Col>
+                  Status: 
+                </Col>
+                <Col>
+                  {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                </Col>
+              </Row>
             </ListGroup.Item>
             <ListGroup.Item>
-              price: ${product.price}
-            </ListGroup.Item>
-            <ListGroup.Item>
-              description: {product.description}
+                  <Button className="btn-block" type='button' disabled={product.countInStock === 0}>
+                    Add to Cart
+                  </Button>
             </ListGroup.Item>
           </ListGroup>
-        </Col>
-        <Col md={3}>
-          <Card>
-            <ListGroup variant='flush'>
-              <ListGroup.Item>
-                <Row>
-                  <Col>
-                    Price: 
-                  </Col>
-                  <Col>
-                    <strong>${product.price}</strong>
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                <Row>
-                  <Col>
-                    Status: 
-                  </Col>
-                  <Col>
-                    {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
-                  </Col>
-                </Row>
-              </ListGroup.Item>
-              <ListGroup.Item>
-                    <Button className="btn-block" type='button' disabled={product.countInStock === 0}>
-                      Add to Cart
-                    </Button>
-              </ListGroup.Item>
-            </ListGroup>
-          </Card>
-        </Col>
-      </Row>
+        </Card>
+      </Col>
+    </Row>}
     </>
   )
 };
